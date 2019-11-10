7c11b520affa0249db373327cee494af781b4dfc
Kmilo9999 <k.diaz99@gmail.com>
2019-11-10 10:49:37 -0500

    init commit

---
+++ src/DisplayManager.java
@@ -0,0 +1,45 @@
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.ATIDrawBuffers;
+import org.lwjgl.opengl.ContextAttribs;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.PixelFormat;
+
+public class DisplayManager {
+   
+	private static final int  WIDTH = 1280;
+	private static final int  HEIGHT = 720;
+	
+	private static final int  FPS = 120;
+	
+	public static void createDisplay() {
+		
+		ContextAttribs attibs = new  ContextAttribs(3,2);
+		attibs.withForwardCompatible(true);
+		attibs.withProfileCore(true);
+		
+		
+		try {
+			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+			Display.create(new PixelFormat(),attibs);
+		} catch (LWJGLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		GL11.glViewport(0, 0, WIDTH, HEIGHT);
+		
+	};
+	
+	public static void updateDisplay() {
+		Display.sync(FPS);
+		Display.update();
+		
+	};
+	public static void closeDisplay() {
+		
+		Display.destroy();
+	};
+	
+}
